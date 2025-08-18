a8b8b98bb8bf3b4afe803b8a42da668881272278
고여경 <2yokyung@naver.com>
2025-08-18 17:25:49 +0900

    [Feat]: 관리자, 기본 말투 seed 설정
    - 4개의 기본 말투 목 데이터 생성.
    - 모든 공유된 말투 목록 조회api에 기본 말투 포함 제거
    - 기본 말투 목록 조회 api추가

--- src/main/java/akkimi_BE/aja/repository/MaltuRepository.java
+++ src/main/java/akkimi_BE/aja/repository/MaltuRepository.java
@@ -5,11 +5,21 @@ import akkimi_BE.aja.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
 import java.util.List;
 
 @Repository
 public interface MaltuRepository extends JpaRepository<Maltu, Long> {
-    List<Maltu> findByIsPublicTrueOrderByCreatedAtDesc();
 
+    //내가 생성한 말투
     List<Maltu> findByCreatorOrderByCreatedAtDesc(User creator);
+
+    //기본 말투 저장되어있는지 확인
+    Integer countByIsDefaultTrue();
+
+    //공개된 말투 리스트 조회(기본 말투 포함x)
+    List<Maltu> findByIsPublicTrueAndIsDefaultFalseOrderByCreatedAtDesc();
+
+    //기본 말투 리스트 조회
+    List<Maltu> findByIsDefaultTrue();
 }
