1c09bd9228eb5c7f311deecd27ace069cc11380b
고여경 <2yokyung@naver.com>
2025-08-21 15:39:01 +0900

    [Fix]: timezone 설정

--- src/main/java/akkimi_BE/aja/AjaApplication.java
+++ src/main/java/akkimi_BE/aja/AjaApplication.java
@@ -2,13 +2,14 @@ package akkimi_BE.aja;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"akkimi_BE.aja", "global"})
 public class AjaApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 		SpringApplication.run(AjaApplication.class, args);
 	}
 
